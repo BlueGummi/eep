42a155962c7621650ebe89ea74959cdc05a6faa8
gummi <105593618+BlueGummi@users.noreply.github.com>
2025-04-01 21:44:48 -0500

    fix stdout reset

--- src/editor/command.rs
+++ src/editor/command.rs
@@ -1,10 +1,16 @@
 use crate::*;
+use std::io::{Write, stdout};
 use std::path::PathBuf;
+
 impl Editor {
     pub fn process_command(&mut self) {
         let cmd = self.command_buffer.trim();
         match cmd {
-            "q" => std::process::exit(0),
+            "q" => {
+                write!(stdout(), "\x1B[?1003l").unwrap();
+                stdout().flush().unwrap();
+                std::process::exit(0);
+            }
             "w" => {
                 if let Err(e) = self.save_file() {
                     self.set_status(&format!("Error saving file: {}", e));
@@ -14,6 +20,8 @@ impl Editor {
                 if let Err(e) = self.save_file() {
                     self.set_status(&format!("Error saving file: {}", e));
                 } else {
+                    write!(stdout(), "\x1B[?1003l").unwrap();
+                    stdout().flush().unwrap();
                     std::process::exit(0);
                 }
             }
